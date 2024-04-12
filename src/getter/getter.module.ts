788d5ac780583044a94d32f233a9f969896d572d
Jorge Sanmiguel <8038323+jsanmigimeno@users.noreply.github.com>
2024-04-12 11:13:39 +0200

    chore: Remove unnecessary imports

--- src/getter/getter.module.ts
+++ src/getter/getter.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from 'src/logger/logger.module';
 import { GetterService } from './getter.service';
 
 @Module({
   providers: [GetterService],
-  imports: [LoggerModule],
 })
 export class GetterModule {}
--- src/submitter/submitter.module.ts
+++ src/submitter/submitter.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from 'src/logger/logger.module';
 import { SubmitterService } from './submitter.service';
 import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
   providers: [SubmitterService],
   exports: [SubmitterService],
-  imports: [LoggerModule, WalletModule],
+  imports: [WalletModule],
 })
 export class SubmitterModule {}
