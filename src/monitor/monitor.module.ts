1c54e9d7b2c5c346583391cceade6d4ddf159c30
Jorge Sanmiguel <8038323+jsanmigimeno@users.noreply.github.com>
2024-04-12 11:12:09 +0200

    fix: Set 'monitor' module as global

--- src/monitor/monitor.module.ts
+++ src/monitor/monitor.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MonitorService } from './monitor.service';
 
+@Global()
 @Module({
     providers: [MonitorService],
     exports: [MonitorService],
