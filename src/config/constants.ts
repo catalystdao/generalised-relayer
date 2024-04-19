5e58d628cb56c07bcbf73364a8370bced5fb271b
Jorge Sanmiguel <8038323+jsanmigimeno@users.noreply.github.com>
2024-04-19 16:00:12 +0000

    chore: Delete unused file

--- src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_CPUS = 'MAX_CPUS';
-export const GAS_MARGIN = 'GAS_MARGIN';
-export const USE_TESTNET = 'USE_TESTNET';
-export const SPY_PORT = 'SPY_PORT';
-export const REDIS_PORT = 'REDIS_PORT';
-export const USING_DOCKER = 'USING_DOCKER';
