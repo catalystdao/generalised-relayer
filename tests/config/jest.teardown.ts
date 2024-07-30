3816218c5dac709dc8fb149dcfb0ac818b408c3b
ajimeno04 <jimenobeltran11@gmail.com>
2024-07-30 14:14:52 +0200

    Fix: Update Jest teardown to work as expected

--- tests/config/jest.teardown.ts
+++ tests/config/jest.teardown.ts
@@ -12,7 +12,7 @@ export default async function globalTeardown() {
     console.log('Running global teardown...');
 
     try {
-        await execPromise('pkill pnpm');
+        await execPromise('pkill -f pnpm');
         console.log('Relayer process stopped.');
     } catch (error) {
         console.error('Failed to stop relayer process:', error);
